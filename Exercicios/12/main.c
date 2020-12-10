60e2d604e52ac48335c6d65c882881c704912996
Gabriel Silva Simoura <55196034+gabrielSSimoura@users.noreply.github.com>
2020-12-10 14:08:36 -0300

    Update main.c

--- Exercicios/12/main.c
+++ Exercicios/12/main.c
@@ -7,12 +7,13 @@ int main(int argc, char const *argv[])
 {
     ListaGen *lista = criaLista();
 
-    Aluno *a1 = InicializaAluno(1, "gabriel", 9.0);
+    Aluno *a1 = InicializaAluno(1, "gabriel", 9.5);
     lista = insereLista(lista, a1);
-    Aluno *a2 = InicializaAluno(13, "emerson", 9.0);
+    
+    Aluno *a2 = InicializaAluno(13, "emerson", 10.0);
     lista = insereLista(lista, a2);
 
-    Aluno *a3 = InicializaAluno(132, "maria", 9.0);
+    Aluno *a3 = InicializaAluno(132, "maria", 10.0);
     lista = insereLista(lista, a3);
 
     Aluno *a4 = InicializaAluno(144, "henriques", 10.0);
@@ -47,7 +48,7 @@ int main(int argc, char const *argv[])
 
     //Retorna a media
     float media = mediaTurma(lista);
-    // printf("\nA media da Turma é: %.2f\n", media); -->^Problema Valgrind
+    // printf("\nA media da Turma é: %.2f\n", media); --> Problema Valgrind
 
     liberaLista(lista);
 
